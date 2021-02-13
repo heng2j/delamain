f715277dd1ed93bfe33cf82148acf31461609a98
codejacktsu <65304002+codejacktsu@users.noreply.github.com>
2021-02-13 00:54:03 -0800

    add train_model

--- lane-tracking/train_model.py
+++ lane-tracking/train_model.py
@@ -31,11 +31,12 @@ def visualize(**images):
 
 
 from torch.utils.data import DataLoader
-from torch.utils.data import Dataset as BaseDataset
+from torch.utils.data import Dataset
+from torch import LongTensor
 
 
-class Dataset(BaseDataset):
-    """CamVid Dataset. Read images, apply augmentation and preprocessing transformations.
+class CarlaDataset(Dataset):
+    """Carla Dataset. Read images, apply augmentation and preprocessing transformations.
 
     Args:
         images_dir (str): path to images folder
@@ -54,6 +55,7 @@ class Dataset(BaseDataset):
             self,
             images_dir,
             masks_dir,
+            classes=None,
             augmentation=None,
             preprocessing=None,
     ):
@@ -62,7 +64,7 @@ class Dataset(BaseDataset):
         self.masks_fps = [os.path.join(masks_dir, image_id.replace(".png", "_label.png")) for image_id in self.ids]
 
         # convert str names to class values on masks
-        self.class_values = [self.CLASSES.index(cls.lower()) for cls in self.CLASSES]
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
 
         self.augmentation = augmentation
         self.preprocessing = preprocessing
@@ -84,13 +86,13 @@ class Dataset(BaseDataset):
             sample = self.preprocessing(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
 
-        return image, mask
+        return image, LongTensor(mask)
 
     def __len__(self):
         return len(self.ids)
 
 
-dataset = Dataset(x_train_dir, y_train_dir)
+dataset = CarlaDataset(x_train_dir, y_train_dir, classes=CarlaDataset.CLASSES)
 
 # image, mask = dataset[30] # get some sample
 # visualize(
@@ -104,14 +106,12 @@ import albumentations as albu
 
 def get_training_augmentation():
     train_transform = [
-
         albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
-
-        albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
-        albu.RandomCrop(height=320, width=320, always_apply=True),
+        # albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
+        # albu.RandomCrop(height=320, width=320, always_apply=True),
 
         albu.IAAAdditiveGaussianNoise(p=0.2),
-        albu.IAAPerspective(p=0.5),
+        # albu.IAAPerspective(p=0.5),
 
         albu.OneOf(
             [
@@ -119,7 +119,7 @@ def get_training_augmentation():
                 albu.RandomBrightnessContrast(p=1),
                 albu.RandomGamma(p=1),
             ],
-            p=0.9,
+            p=0.6,
         ),
 
         albu.OneOf(
@@ -128,7 +128,7 @@ def get_training_augmentation():
                 albu.Blur(blur_limit=3, p=1),
                 albu.MotionBlur(blur_limit=3, p=1),
             ],
-            p=0.9,
+            p=0.6,
         ),
 
         albu.OneOf(
@@ -136,7 +136,7 @@ def get_training_augmentation():
                 albu.RandomBrightnessContrast(p=1),
                 albu.HueSaturationValue(p=1),
             ],
-            p=0.9,
+            p=0.6,
         ),
     ]
     return albu.Compose(train_transform)
@@ -163,17 +163,18 @@ def get_preprocessing(preprocessing_fn):
 
     _transform = [
         albu.Lambda(image=preprocessing_fn),
-        albu.Lambda(image=to_tensor, mask=to_tensor),
+        albu.Lambda(image=to_tensor),
     ]
     return albu.Compose(_transform)
 
 
 #### Visualize resulted augmented images and masks
 
-augmented_dataset = Dataset(
+augmented_dataset = CarlaDataset(
     x_train_dir,
     y_train_dir,
-    augmentation=get_training_augmentation()
+    augmentation=get_training_augmentation(),
+    classes=CarlaDataset.CLASSES
 )
 
 # same image with different random transforms
@@ -189,7 +190,7 @@ import segmentation_models_pytorch as smp
 
 ENCODER = 'efficientnet-b0'
 ENCODER_WEIGHTS = 'imagenet'
-CLASSES = Dataset.CLASSES
+CLASSES = CarlaDataset.CLASSES
 ACTIVATION = 'softmax2d'
 DEVICE = 'cuda'
 
@@ -204,32 +205,148 @@ model = smp.FPN(
 preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
 
 
-train_dataset = Dataset(
+train_dataset = CarlaDataset(
     x_train_dir,
     y_train_dir,
     augmentation=get_training_augmentation(),
     preprocessing=get_preprocessing(preprocessing_fn),
-    # classes=CLASSES,
+    classes=CarlaDataset.CLASSES,
 )
 
-valid_dataset = Dataset(
+valid_dataset = CarlaDataset(
     x_valid_dir,
     y_valid_dir,
     augmentation=get_validation_augmentation(),
     preprocessing=get_preprocessing(preprocessing_fn),
-    # classes=CLASSES,
+    classes=CarlaDataset.CLASSES,
 )
 
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=12)
-valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
-
-class MultiClassDiceLoss(smp.utils.losses.DiceLoss):
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=2, shuffle=False)
 
 
-loss = smp.utils.losses.DiceLoss()
+from segmentation_models_pytorch.utils import base
+from segmentation_models_pytorch.utils.losses import DiceLoss
+
+
+label_left = CarlaDataset.CLASSES.index('left_boundary')
+label_right = CarlaDataset.CLASSES.index('right_boundary')
+
+class MultiClassDiceLoss(base.Loss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.BinaryDiceLossLeft = DiceLoss()
+        self.BinaryDiceLossRight = DiceLoss()
+
+    def forward(self, y_pr, y_gt):
+        left_gt = (y_gt == label_left)
+        left_loss = self.BinaryDiceLossLeft.forward(y_pr[:, label_left, :, :], left_gt)
+        right_gt = (y_gt == label_right)
+        right_loss = self.BinaryDiceLossRight.forward(y_pr[:, label_right, :, :], right_gt)
+        return 0.5*(left_loss + right_loss)
+
+
+loss = MultiClassDiceLoss()
 metrics = []
 
 optimizer = torch.optim.Adam([
     dict(params=model.parameters(), lr=0.0001),
 ])
 
+
+# create epoch runners
+# it is a simple loop of iterating over dataloader`s samples
+train_epoch = smp.utils.train.TrainEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    optimizer=optimizer,
+    device=DEVICE,
+    verbose=True,
+)
+
+valid_epoch = smp.utils.train.ValidEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    device=DEVICE,
+    verbose=True,
+)
+
+
+def main():
+    # train model for 40 epochs
+    max_score = 1e10
+    epochs_n = 5
+    for i in range(0, epochs_n):
+
+        print('\nEpoch: {}'.format(i))
+        train_logs = train_epoch.run(train_loader)
+        valid_logs = valid_epoch.run(valid_loader)
+
+        # do something (save model, change lr, etc.)
+        if max_score > valid_logs['multi_class_dice_loss']:
+            max_score = valid_logs['multi_class_dice_loss']
+            torch.save(model, './best_model.pth'.format('multi_class_dice_loss'))
+            print('Model saved!')
+
+        if i == 3:
+            optimizer.param_groups[0]['lr'] = 1e-5
+            print('Decrease decoder learning rate to 1e-5!')
+
+# if __name__ == '__main__':
+#     main()
+
+# load best saved checkpoint
+best_model = torch.load('./best_model.pth')
+
+test_best_model = False
+if test_best_model:
+    # create test dataset
+    test_dataset = CarlaDataset(
+        x_valid_dir,
+        y_valid_dir,
+        augmentation=get_validation_augmentation(),
+        preprocessing=get_preprocessing(preprocessing_fn),
+        classes=CarlaDataset.CLASSES,
+    )
+
+    test_dataloader = DataLoader(test_dataset)
+
+    # evaluate model on test set
+    test_epoch = smp.utils.train.ValidEpoch(
+        model=best_model,
+        loss=loss,
+        metrics=metrics,
+        device=DEVICE,
+    )
+
+    logs = test_epoch.run(test_dataloader)
+
+
+# test dataset without transformations for image visualization
+test_dataset_vis = CarlaDataset(
+    x_valid_dir, y_valid_dir,
+    classes=CarlaDataset.CLASSES,
+    preprocessing=get_preprocessing(preprocessing_fn)
+)
+
+for i in range(3):
+    n = np.random.choice(len(test_dataset_vis))
+
+    image_vis = test_dataset_vis[n][0].astype('uint8')
+    image, gt_mask = test_dataset_vis[n]
+
+    x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
+    pr_mask_left = best_model.predict(x_tensor)[0, 1, :, :]
+    pr_mask_left = (pr_mask_left.cpu().numpy())
+
+    pr_mask_right = best_model.predict(x_tensor)[0, 2, :, :]
+    pr_mask_right = (pr_mask_right.cpu().numpy())
+
+    visualize(
+        ground_truth_mask=gt_mask,
+        predicted_mask_left=pr_mask_left,
+        predicted_mask_right=pr_mask_right
+    )
+    
