693be15497575d8fffc4910bce585eb1835f4f33
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2021-03-16 21:00:42 -0700

    Updated gnss.py

--- gps_nav/gnss.py
+++ gps_nav/gnss.py
@@ -5,19 +5,22 @@ import math
 
 
 def gnss_live_location(event):
-    """Get the GNSS Measurements
-    :return: Values of Altitude, Latitude, and Longitude
-    :rtype: float
+    """Get and store the GNSS Measurements.
+    :return: Float values of Altitude, Latitude, and Longitude
     """
-    alt = event.altitude
-    lat = event.latitude
-    lon = event.longitude
+    alt = event.altitude  # meters
+    lat = event.latitude  # degrees
+    lon = event.longitude  # degrees
+
     # print("GNSS measure:\n" + str(event) + '\n')  # For full details of measurements incl. frame and timestamp
     print("altitude:", str(alt))
     print("latitude:", str(lat))
     print("longitude:", str(lon), "\n")
+
     return alt, lat, lon
 
+
+# ORIENTATION OF THE CAR???
 
 # Actual location
 # vehicle_location = ...
