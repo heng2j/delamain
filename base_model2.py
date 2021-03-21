fb508acd4fc308e8f4ef12276906a98063044711
codejacktsu <65304002+codejacktsu@users.noreply.github.com>
2021-03-21 07:04:03 -0700

    update

--- base_model2.py
+++ base_model2.py
@@ -22,6 +22,7 @@ import pygame
 from base.hud import HUD
 from base.world import World
 from base.manual_control import KeyboardControl
+from base.debug_cam import process_img
 
 
 # ==============================================================================
@@ -45,6 +46,24 @@ def game_loop(args):
         hud = HUD(args.width, args.height)
         world = World(client.get_world(), hud, args)
         controller = KeyboardControl(world, False)
+
+        #TODO - add sensors
+        blueprint_library = world.world.get_blueprint_library()
+
+        # Camera RGB sensor
+        bp_cam_rgb = blueprint_library.find('sensor.camera.rgb')
+        bp_cam_rgb.set_attribute('image_size_x', '1024')
+        bp_cam_rgb.set_attribute('image_size_y', '512')
+        bp_cam_rgb.set_attribute('fov', '110')
+        # bp_cam_rgb.set_attribute('sensor_tick', '1.0')
+
+        # Spawn Sensors
+        transform = carla.Transform(carla.Location(x=2.5, z=0.7))
+        cam_rgb = world.world.spawn_actor(bp_cam_rgb, transform, attach_to=world.player)
+        print('created %s' % cam_rgb.type_id)
+
+        # Activate Sensors
+        cam_rgb.listen(lambda data: process_img(data))
 
         clock = pygame.time.Clock()
         while True:
